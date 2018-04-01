3fc7a963e4d7b12c645e31113ac355ff7f5e41f6
MF <medferjani.elifa@gmail.com>
2018-04-01 20:50:35 +0100

    Test unitaire create a category

---
+++ tests/AppBundle/Controller/ApiProjectControllerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+//use PHPUnit\Framework\TestCase;
+
+class ApiProjectControllerTest extends WebTestCase
+{
+    /**
+     * Test create category - api format Json
+     * @group Functional
+     */
+    public function testPostCategory()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/api/category', ['name' => 'Category 1']);
+        
+        $jsonCrawler = json_decode($client->getResponse()->getContent());
+       
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $jsonCrawler->code);
+    }
+
+    /**
+     * Test create category - api format Xml
+     * @group Functional
+     */
+    public function testPostCategoryXml()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/api/category.xml', ['name' => 'Category 1']);
+        
+        $xmlCrawler = new Crawler(str_replace('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>', '',$client->getResponse()->getContent()));
+
+        $entrys = $xmlCrawler->filterXPath('//result/entry')->extract(array('_text'));
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $entrys[1]);
+    }
+
+    /**
+     * Test create category without name - api Json
+     * @group Functional
+     */
+    public function testPostCategoryWithoutName()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/api/category');
+        
+        $jsonCrawler = json_decode($client->getResponse()->getContent());
+       
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(403, $jsonCrawler->code);
+    }
+
+
+    /**
+     * Test create category without name - api Xml
+     * @group Functional
+     */
+    public function testPostCategoryWithoutNameXml()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/api/category.xml');
+        
+        $xmlCrawler = new Crawler(str_replace('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>', '',$client->getResponse()->getContent()));
+
+        $entrys = $xmlCrawler->filterXPath('//result/entry')->extract(array('_text'));
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(403, $entrys[1]);
+    }
+
+   
+}
